7b63109e246ef2811a793d941ea3379fea9efbea
Kuber Shahi <kuber.shahi_ug21@ashoka.edu.in>
2021-01-01 22:12:06 +0530

    Todo item

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,16 +1,19 @@
 <template>
-    <div>
-        <div v-bind:key = "todo.id" v-for="todo in todos">
-            <h3>{{todo.title}}</h3>
-        </div>
+  <div>
+    <div v-bind:key = "todo.id" v-for="todo in todos">
+      <TodoItem />
     </div>
+  </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue'
 export default {
     name: "Todos",
+    components: {
+      TodoItem,
+    },
     props: ["todos"]
-
 }
 </script>
 
